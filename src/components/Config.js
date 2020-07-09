25e56f370b4a4f8f68a1626b054cd2660cfd4663
princy garg <princygarg28@gmail.com>
2020-07-10 01:03:14 +0530

    Config updated

--- src/components/Config.js
+++ src/components/Config.js
@@ -4,10 +4,10 @@ import 'firebase/firestore';
 // const settings= {timestampsInSnapshots: true}
 
 var firebaseConfig = {
-        apiKey: "process.env.REACT_APP_FIREBASE_apiKey",
+        apiKey: process.env.REACT_APP_FIREBASE_apiKey,
         authDomain: "process.env.REACT_APP_FIREBASE_authDomain",
         databaseURL: "process.env.REACT_APP_FIREBASE_databaseURL",
-        projectId: "process.env.REACT_APP_FIREBASE_projectId",
+        projectId: process.env.REACT_APP_FIREBASE_projectId,
         storageBucket: "process.env.REACT_APP_FIREBASE_storageBucket",
         messagingSenderId: "process.env.REACT_APP_FIREBASE_messagingSenderId",
         appId: "process.env.REACT_APP_FIREBASE_appId",
