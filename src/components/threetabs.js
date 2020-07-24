7ba3f5829ee0c122ff72093d804176d47fef7060
Geet Agarwal <17ucs059@lnmiit.ac.in>
2020-07-24 19:14:29 +0530

    geet

--- src/components/threetabs.js
+++ src/components/threetabs.js
@@ -6,27 +6,61 @@ const { TabPane } = Tabs;
 
 class ThreeTabs extends React.Component {
   // constructor(props){
-  //   super(props);
-  // }
+  //    super(props);
+  //  }
   render() {
     return (
       <div>
         <Tabs tabPosition="left">
           <TabPane tab="New offers" key="1">
-          <h5>Here are new offers from your interests: </h5>
+            <div className="col-sm-5">
+            <h5>Here are new offers from your interests: </h5>
+              { this.props.propnotifs.map(notif=> {
+
+                const elapsedTime = ((notif.time).valueOf() - notif.logTime)
+                console.log("OfferTime is "+ (notif.time).valueOf() + " and LogTime is "+notif.logTime)
+                console.log ("Elapsed time is "+ elapsedTime)
+
+                return (
+                  (notif.logTime <= (notif.time).valueOf()) ? 
+                  (	
+                    <div className="card-post mb-4 card card-small">
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {notif.Name}
+                        </h5>
+                        <img src= {notif.imageurl} alt="DealArena" width="100px" height="100px"/>
+                        <h5 className="card-title"> {notif.Description}</h5>
+
+                        <h5 className="card-title">Category: {notif.Category}</h5>
+                      </div>
+
+                      <div className="border-top d-flex card-footer">
+                      <div className="card-post__author d-flex">
+                        <a href="/" className="card-post__author-avatar card-post__author-avatar--small" >
+                          Offer: {notif.Offer} </a>
+                      <div className="d-flex flex-column justify-content-center ml-3"><span className="card-post__author-name">Rs.{notif.Price}</span><small className="text-muted"> Offer expires {notif.Expiry}</small></div></div><div className="my-auto ml-auto"><a href={notif.producturl}> BUY NOW</a></div></div>
+                    </div>
+                  ) : ( 
+                    <span> { "" } </span>
+                  )	
+                )
+              })}
+            </div>  
 
           </TabPane>
+
           <TabPane tab="Old offers" key="2">
-          <div className="col-sm-5">
+            <div className="col-sm-5">
 							<h5>Here are old offers from your interests: </h5>
-							{this.props.propoffers.map(offer=>
+							{/* {this.props.propoffers.map(offer=>
 								<div className="card-post mb-4 card card-small">
 								<div className="card-body">
 									<h5 className="card-title">
 										{offer.Name}
 									</h5>
 									<img src= {offer.imageurl} alt="DealArena" width="100px" height="100px"/>
-									<h5 className="card-title"> {offer.Description}</h5>					
+									<h5 className="card-title"> {offer.Description}</h5>
 
 									<h5 className="card-title">Category: {offer.Category}</h5>
 								</div>
@@ -37,9 +71,41 @@ class ThreeTabs extends React.Component {
 										Offer: {offer.Offer} </a>
 								<div className="d-flex flex-column justify-content-center ml-3"><span className="card-post__author-name">Rs.{offer.Price}</span><small className="text-muted"> Offer expires {offer.Expiry}</small></div></div><div className="my-auto ml-auto"><a href={offer.producturl}> BUY NOW</a></div></div></div>
 							)}
-						</div>
+						</div> */}
+            { this.props.propnotifs.map(notif=> {
 
+              const elapsedTime = ((notif.time).valueOf() - notif.logTime)
+              console.log("OfferTime is "+ (notif.time).valueOf() + " and LogTime is "+notif.logTime)
+              console.log ("Elapsed time is "+ elapsedTime)
+
+              return (
+                (notif.logTime > (notif.time).valueOf()) ? 
+                (	
+                  <div className="card-post mb-4 card card-small">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {notif.Name}
+                      </h5>
+                      <img src= {notif.imageurl} alt="DealArena" width="100px" height="100px"/>
+                      <h5 className="card-title"> {notif.Description}</h5>
+
+                      <h5 className="card-title">Category: {notif.Category}</h5>
+                    </div>
+
+                    <div className="border-top d-flex card-footer">
+                    <div className="card-post__author d-flex">
+                      <a href="/" className="card-post__author-avatar card-post__author-avatar--small" >
+                        Offer: {notif.Offer} </a>
+                    <div className="d-flex flex-column justify-content-center ml-3"><span className="card-post__author-name">Rs.{notif.Price}</span><small className="text-muted"> Offer expires {notif.Expiry}</small></div></div><div className="my-auto ml-auto"><a href={notif.producturl}> BUY NOW</a></div></div>
+                  </div>
+                ) : ( 
+                  <span> { "" } </span>
+                )	
+              )
+              })}
+            </div>
           </TabPane>
+          
           <TabPane tab="Other offers" key="3">
           <h5>Here are all other offers: </h5>
 
--- src/components/userhome.js
+++ src/components/userhome.js
@@ -1,3 +1,4 @@
+
 import React,{Component} from 'react';
 // import Sidebar from "./sidebar.js"
 import firebase from "./Config";
@@ -5,7 +6,6 @@ import history from './../history';
 import TreeCheck from './treecheck';
 import moment from 'moment';
 import ThreeTabs from './threetabs';
-
 //import { findAllByPlaceholderText } from '@testing-library/react';
 
 
@@ -134,6 +134,14 @@ class userhome extends Component{
 			notifs.push({
 				key:doc.id,
 				doc,
+				Brand,
+				Name,
+				Description,
+				Price,
+				Category,
+				Expiry,
+				Offer,
+				imageurl,
 				producturl,
 				content: 'New Offer: ',
             	offerD: `${Brand} ${Category} ${Offer}`,
@@ -254,7 +262,29 @@ class userhome extends Component{
                 	
 					<div className="col-lg-8">
 					<div className="row">
-                    <ThreeTabs propoffers={this.state.offers}/>
+					<ThreeTabs propoffers={this.state.offers}
+					propnotifs={this.state.notifs}/>
+						{/* <div className="col-sm-5">
+							<h5>Here are offers from your interests: </h5>
+							{this.state.offers.map(offer=>
+								<div className="card-post mb-4 card card-small">
+								<div className="card-body">
+									<h5 className="card-title">
+										{offer.Name}
+									</h5>
+									<img src= {offer.imageurl} alt="DealArena" width="100px" height="100px"/>
+									<h5 className="card-title"> {offer.Description}</h5>					
+
+									<h5 className="card-title">Category: {offer.Category}</h5>
+								</div>
+
+								<div className="border-top d-flex card-footer">
+								<div className="card-post__author d-flex">
+									<a href="/" className="card-post__author-avatar card-post__author-avatar--small" >
+										Offer: {offer.Offer} </a>
+								<div className="d-flex flex-column justify-content-center ml-3"><span className="card-post__author-name">Rs.{offer.Price}</span><small className="text-muted"> Offer expires {offer.Expiry}</small></div></div><div className="my-auto ml-auto"><a href={offer.producturl}> BUY NOW</a></div></div></div>
+							)}
+						</div> */}
 						{/* <Sidebar/> */}
 
 	  				</div>
