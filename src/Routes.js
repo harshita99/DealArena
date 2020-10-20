203400b51da44e69f50463cfca7796ee972d0c7f
Geet Agarwal <17ucs059@lnmiit.ac.in>
2020-10-20 13:28:03 +0530

    update

--- src/Routes.js
+++ src/Routes.js
@@ -6,14 +6,14 @@ import Home from "./components/Home";
 import i1signup from "./components/i1signup";
 import i2signup from "./components/i2signup";
 import userhome from "./components/userhome";
-import productownerhome from "./components/productownerhome";
+// import productownerhome from "./components/productownerhome";
 import addoffer from "./components/AddOffer";
 import addproduct from "./components/AddProduct";
 import showproduct from "./components/ShowProduct";
 import updateoffer from "./components/UpdateOffer";
 import manageoffers from "./components/ManageOffers";
 import updateproduct from "./components/UpdateProduct";
-import add from "./components/add";
+// import add from "./components/add";
 import nav from "./components/NavBar"
 import history from './history';
 
@@ -26,14 +26,14 @@ export default class Routes extends Component {
                     <Route path="/i1signup" component={i1signup} />
                     <Route path="/i2signup" component={i2signup} />
                     <Route path="/userhome" component={userhome}/>
-                    <Route path="/productownerhome" component={productownerhome}/>
+                    {/* <Route path="/productownerhome" component={productownerhome}/> */}
                     <Route path="/addoffer" component={addoffer}/>
                     <Route path="/addproduct" component={addproduct}/>                  
                     <Route path="/showproduct" component={showproduct}/>
                     <Route path="/updateoffer" component={updateoffer}/>
                     <Route path="/manageoffers" component={manageoffers}/>
                     <Route path="/updateproduct" component={updateproduct}/>
-                    <Route path="/add" component={add}/>
+                    {/* <Route path="/add" component={add}/> */}
                     <Route path="/nav" component={nav}/>
                 </Switch>
             </Router>
